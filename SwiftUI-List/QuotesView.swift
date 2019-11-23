161eef03ec1093763454e364fce698933947f468
Agustin Errecalde <agustinerrecalde@MacBook-Pro-de-Agustin.local>
2019-11-23 16:21:49 -0300

    Se agrega doble pegada a los quotes

--- SwiftUI-List/QuotesView.swift
+++ SwiftUI-List/QuotesView.swift
@@ -15,18 +15,36 @@ import SwiftyJSON
 struct QuotesView: View {
     
     @ObservedObject var service = QuoteDAO()
+    @ObservedObject var service2 = QuoteDAO()
+    
+    var quotes: [Quotes] {
+        service.quotes + service2.quotes
+    }
+    
+    
     
     var body: some View {
        
-        List(service.quotes, id: \.quote) { item in
-            CardQuote(nameAPI: item.character, quote: item.quote, image: item.image)
-        }.navigationBarTitle(Text("Twittson"))
+        List(quotes, id: \.quote) { item in
+            ZStack(alignment: .leading){
+                Rectangle()
+                    .foregroundColor(Color(red: 247/256, green: 206/256, blue: 70/256))
+                    .cornerRadius(10)
+                    .shadow(color: .gray,radius: 5)                    
+                
+                CardQuote(nameAPI: item.character, quote: item.quote, image: item.image)
+            }.padding(.all, 5)
+        
+        
+        }
+            
+        .navigationBarTitle(Text("Twittson"))
     }
 }
 
 
 struct QuotesView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        QuotesView()
     }
 }
